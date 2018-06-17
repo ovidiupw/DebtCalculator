3ca74d967e041cb0e0d8c6556eb6ef7266fe6e99
Ovidiu Pricop <ovidiu.pricop@gmail.com>
2018-06-17 19:28:14 +0300

    Add dependency--injection via typedi

--- src/debt-calculator.ts
+++ src/debt-calculator.ts
@@ -1,9 +1,15 @@
+import "reflect-metadata"; // Configure the DI framework: https://github.com/typestack/typedi#usage-with-typescript
+import {Service, Inject} from "typedi";
+
 import logger from './lib/log/logger';
 
+@Service("DebtCalculator")
 class DebtCalculator {
-  constructor(message: string) {
+
+  constructor(@Inject('DebtCalculator.Message') message: string) {
     logger.info(message);
   }
+
 }
 
 export default DebtCalculator;
--- tst/debt-calculator.test.ts
+++ tst/debt-calculator.test.ts
@@ -1,7 +1,11 @@
 import DebtCalculator from '../src/debt-calculator';
+import {Container} from "typedi";
 
 describe('Debt Calculator', () => {
   it('Should log the message passed into the constructor', () => {
-    new DebtCalculator('Hello world!');
+    Container.set([
+      {id: 'DebtCalculator.Message', value: 'Hello world! DI is working!'}
+    ])
+    Container.get(DebtCalculator);
   });
 });
